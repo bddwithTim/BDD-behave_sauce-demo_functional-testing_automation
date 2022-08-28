c5437b732c4d46441e360e7d1ddc63fab53aef22
Sourcery AI <>
2022-08-28 13:55:42 +0000

    'Refactored by Sourcery'

--- steps/inventory.py
+++ steps/inventory.py
@@ -10,16 +10,18 @@ from sauce_demo_bdd.context import SauceDemoContext
 @when("I sort the products by {sort_order} order")
 def step_impl_1(context: SauceDemoContext, sort_order: str):
     sort_option = context.driver.find_element_by_xpath(
-        "//option[contains(text(),'{}')]".format(sort_order)
+        f"//option[contains(text(),'{sort_order}')]"
     )
+
     sort_option.click()
 
 
 @then("the products page is sorted in {sort_order} order")
 def step_impl_2(context: SauceDemoContext, sort_order: str):
     product_sort_container = context.driver.find_element_by_xpath(
-        "//span[contains(text(),'{}')]".format(sort_order)
+        f"//span[contains(text(),'{sort_order}')]"
     )
+
     expect(product_sort_container.text.lower()).to(equal(sort_order.lower()))
 
 
